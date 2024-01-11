9849f968d2027bdf7bd047b8c4541516089b9d6d
IO <ivanopeidawork@gmail.com>
2024-01-11 00:33:18 +0000

    add basic infra code

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "gcs" {
+    bucket = "io-tf-states"
+    prefix = "terraform/state"
+  }
+}
+
+module "gke_cluster" {
+  source         = "github.com/io-err/tf-google-gke-cluster"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = 2
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "GOOGLE_REGION" {
+  type        = string
+  description = "Region for cluster deployment"
+}
+
+variable "GOOGLE_PROJECT" {
+  type        = string
+  description = "Project name for cluster deployment"
+
+}
